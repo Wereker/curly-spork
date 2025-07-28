d494ee2c946607b49314c8944d439365966bde89
Wereker <werkermatvey@gmail.com>
2025-07-28 19:24:51 +0700

    add method to connect Database and migrate models

---
+++ internal/db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"app/internal/models"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load("deployments/.env.dev")
+		if err != nil {
+			log.Println("Не удалось загрузить .env.dev:", err)
+		}
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Не удалось подключиться к БД: " + err.Error())
+	}
+
+	db.AutoMigrate(&models.Measure{}, &models.Product{})
+
+	fmt.Print("Штатно отработало")
+	return db
+}
