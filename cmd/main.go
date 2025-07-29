8022c50c69be0a4a80682cbda011a3f8909ebc3a
Wereker <werkermatvey@gmail.com>
2025-07-29 12:32:36 +0700

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"app/internal/db"
+	"app/internal/routes"
+
+	_ "app/docs"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+// @title Test API
+// @version 1.0
 func main() {
-	db.Connect()
+	database := db.Connect()
+	app := fiber.New()
+
+	routes.Setup(app, database)
+
+	app.Listen(":3000")
 }
